19c89b971ae43f6ba1882f02265c1ff00198f1e0
unknown <matheo.abellar@gmail.com>
2023-07-21 12:21:53 -0500

    paises

--- main.py
+++ main.py
@@ -1,9 +1,109 @@
+ 
 
-from flask import Flask
+from flask import Flask, render_template 
 
-app = Flask(__name__)
+ 
+ 
 
-@app.route ('/')
+app = Flask(__name__) 
 
-def index():
-    return 'Hola 2687386'
+ 
+ 
+
+@app.route ('/hola') 
+
+ 
+ 
+
+def hola(): 
+
+    return 'Hola' 
+
+@app.route('/paises') 
+
+def paises(): 
+
+     
+
+    paises=[ 
+
+                [ 
+
+                    { 
+
+                        "nombre":"America", 
+
+                        'paises':[ 
+
+                                    "Colombia", 
+
+                                    "Peru" 
+
+                                ] 
+
+                    } 
+
+                ], 
+
+                [ 
+
+                    { 
+
+                        "nombre":"Asia", 
+
+                        'paises':[ 
+
+                                    "Japon", 
+
+                                    "Irlanda", 
+
+                                    "Corea" 
+
+                                ] 
+
+                    } 
+
+                ]  
+
+            ] 
+
+    ''''variables de conteo para cada continente''' 
+
+     
+
+    contador_paises_america =0 
+
+    for pais in paises[0][0]['paises']: 
+
+        contador_paises_america= contador_paises_america+1 
+
+        print(contador_paises_america) 
+
+         
+
+ 
+ 
+
+    contador_paises_asia =0 
+
+    for pais in paises[1][0]['paises']: 
+
+        contador_paises_asia= contador_paises_asia+1 
+
+        print(contador_paises_asia) 
+
+     
+
+         
+
+    user ='Valentina Guzman' 
+
+    return render_template('paises_listas.html', 
+
+                           user= user, 
+
+                           continentes= paises, 
+
+                           Asia=contador_paises_asia, 
+
+                            America=contador_paises_america )
